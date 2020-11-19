b0d0ff439e9cb47f0f4970ddaf307ca807ab5474
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-11-19 18:27:56 -0500

    Update employeeSQL.sql

--- EmployeeSQL/employeeSQL.sql
+++ EmployeeSQL/employeeSQL.sql
@@ -12,3 +12,16 @@ select first_name, last_name, hire_date
 from employees
 where extract(year from hire_date) = 1986; 
 
+--3. List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+select a.dept_no as "Department Number"
+      , b.dept_name as "Department Name"
+	  , a.emp_no as "Employee Number"
+	  , c.last_name as "Last Name"
+	  , c.first_name as "First Name"
+from dept_manager a
+join departments b on a.dept_no = b.dept_no
+join employees c on a.emp_no = c.emp_no 
+where c.emp_title_id in (select title_id from titles where title = 'Manager')
+order by a.dept_no
+
