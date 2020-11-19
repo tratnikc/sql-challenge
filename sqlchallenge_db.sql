70ad951c2205380fb08590566ffce1786466c8e8
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-11-19 13:16:31 -0500

    create database

---
+++ sqlchallenge_db.sql
@@ -0,0 +1,11 @@
+-- create database sql-challenge
+CREATE DATABASE "sql-challenge"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+-- create table departments
