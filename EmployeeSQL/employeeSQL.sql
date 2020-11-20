aea7823622a65463c646e95faa6efe134765fdf1
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-11-19 22:30:51 -0500

    Update employeeSQL.sql

--- EmployeeSQL/employeeSQL.sql
+++ EmployeeSQL/employeeSQL.sql
@@ -5,7 +5,8 @@ select a.emp_no as "Employee Number"
 	  , sex as "Sex"
 	  , salaries as "Salary"
 from employees a
-join salaries b on b.emp_no = a.emp_no;
+join salaries b 
+    on b.emp_no = a.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 select first_name, last_name, hire_date
@@ -20,8 +21,23 @@ select a.dept_no as "Department Number"
 	  , c.last_name as "Last Name"
 	  , c.first_name as "First Name"
 from dept_manager a
-join departments b on a.dept_no = b.dept_no
-join employees c on a.emp_no = c.emp_no 
+join departments b 
+   on a.dept_no = b.dept_no
+join employees c 
+   on a.emp_no = c.emp_no 
 where c.emp_title_id in (select title_id from titles where title = 'Manager')
-order by a.dept_no
+order by a.dept_no;
+
+-- 4. List the department of each employee with the following information: 
+--    employee number, last name, first name, and department name.
+select a.emp_no
+      , a.last_name
+	  , a.first_name
+	  , c.dept_name
+from employees a
+join dept_emp b 
+   on a.emp_no = b.emp_no
+join departments c
+   on b.dept_no = c.dept_no;
+
 
