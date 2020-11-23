ee6c7778cd2277e4562c858423d7275351a91584
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-11-23 15:12:58 -0500

    Update employeeSQL.sql

--- EmployeeSQL/employeeSQL.sql
+++ EmployeeSQL/employeeSQL.sql
@@ -1,9 +1,9 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary 
 select a.emp_no as "Employee Number"
-      , last_name as "Last Name"
-	  , first_name as "First Name"
-	  , sex as "Sex"
-	  , salaries as "Salary"
+      , a.last_name as "Last Name"
+	  , a.first_name as "First Name"
+	  , a.sex as "Sex"
+	  , b.salaries as "Salary"
 from employees a
 join salaries b 
     on b.emp_no = a.emp_no;
@@ -14,10 +14,10 @@ from employees
 where extract(year from hire_date) = 1986; 
 
 --3. List the manager of each department with the following information: 
---department number, department name, the manager's employee number, last name, first name.
+--   department number, department name, the manager's employee number, last name, first name.
 select a.dept_no as "Department Number"
       , b.dept_name as "Department Name"
-	  , a.emp_no as "Employee Number"
+	  , c.emp_no as "Managers Employee Number"
 	  , c.last_name as "Last Name"
 	  , c.first_name as "First Name"
 from dept_manager a
@@ -30,15 +30,15 @@ order by a.dept_no;
 
 -- 4. List the department of each employee with the following information: 
 --    employee number, last name, first name, and department name.
-select a.emp_no
-      , a.last_name
-	  , a.first_name
+select b.emp_no
+      , b.last_name
+	  , b.first_name
 	  , c.dept_name
-from employees a
-join dept_emp b 
-   on a.emp_no = b.emp_no
+from dept_emp a
+join employees b 
+   on b.emp_no = a.emp_no
 join departments c
-   on b.dept_no = c.dept_no;
+   on c.dept_no = a.dept_no;
 
 -- 5. List first name, last name, and sex for employees whose 
 --    first name is "Hercules" and last names begin with "B."
@@ -51,30 +51,31 @@ where first_name = 'Hercules'
 
 -- 6. List all employees in the Sales department, including their 
 --    employee number, last name, first name, and department name.
-select a.emp_no
-      , a.last_name
-	  , a.first_name
+select b.emp_no
+      , b.last_name
+	  , b.first_name
 	  , c.dept_name as "Department Name"
-from employees a
-join dept_emp b 
-   on a.emp_no = b.emp_no
+from dept_emp a
+join employees b 
+   on b.emp_no = a.emp_no
 join departments c
-   on b.dept_no = c.dept_no
+   on c.dept_no = a.dept_no
 where c.dept_name = 'Sales';
 
 
 -- 7. List all employees in the Sales and Development departments, 
 --    including their employee number, last name, first name, and department name.
-select a.emp_no
-      , a.last_name
-	  , a.first_name
+select b.emp_no
+      , b.last_name
+	  , b.first_name
 	  , c.dept_name as "Department Name"
-from employees a
-join dept_emp b 
-   on a.emp_no = b.emp_no
+from dept_emp a
+join employees b 
+   on b.emp_no = a.emp_no
 join departments c
-   on b.dept_no = c.dept_no
+   on c.dept_no = a.dept_no
 where c.dept_name in ('Sales', 'Development');
+
 
 -- 8. In descending order, list the frequency count of employee last names, 
 --    i.e., how many employees share each last name.
